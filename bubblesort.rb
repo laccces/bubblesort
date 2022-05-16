3281652df754017e288bdccd005fa73e853533d0
Dan <en07djc@icloud.com>
2022-05-16 09:29:14 +0100

    Fixed looping issue.

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,19 +1,20 @@
 def bubble_sort(array)
-  n = array.count
-  i = 0
-  loop_n = n - 1
 
-  loop_n.times do # if you put in any more than 5, it breaks when it tries to run array[6], which doesn't exist
-   if array[i] > array[i + 1]
-    array[i], array[i + 1] = array[i + 1], array[i]
+for i in 0...array.length
+  sorted = true
+  
+  for x in 0...(array.length - i - 1) 
+    if array[x] > array[x + 1]
+      array[x], array[x + 1] = array[x + 1], array[x]
+      sorted = false
     end
-  i +=1
   end
+
+  break if sorted
+end
 
 p array
 
 end
 
 bubble_sort([4,3,78,2,0,2])
-
-# it's looping through once and then stopping
